76ea1303fc9b94a5dcf09979ee75267abdf5e8fa
wzyonggege <jamwong994@gmail.com>
2018-02-21 21:57:07 +0800

    fix

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from django.db.models import Q
 
 from .models import Post, Category
 from comments.forms import CommentForm
 
 import markdown
+
+
+def archives(request):
+    date_list = Post.objects.dates('created_time', 'month', order='DESC')
+    return render(request, 'archives.html', context={'date_list': date_list})
 
 
 class IndexView(ListView):
@@ -20,15 +24,27 @@ class ArchivesView(IndexView):
         return super(ArchivesView, self).get_queryset().filter(created_time__year=self.kwargs.get('year'),
                                                                created_time__month=self.kwargs.get('month'),)
 
-def archives(request):
-    date_list = Post.objects.dates('created_time', 'month', order='DESC')
-    return render(request, 'archives.html', context={'date_list': date_list})
+    def get_context_data(self, **kwargs):
+        context = super(ArchivesView, self).get_context_data(**kwargs)
+        title = '{} 年 {} 月'.format(self.kwargs.get('year'), self.kwargs.get('month'))
+        context.update({
+            'title': title
+        })
+        return context
 
 
 class CategoryView(IndexView):
     def get_queryset(self):
         cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
         return super(CategoryView, self).get_queryset().filter(category=cate)
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryView, self).get_context_data(**kwargs)
+        title = '{}'.format(get_object_or_404(Category, pk=self.kwargs.get('pk')))
+        context.update({
+            'title': title
+        })
+        return context
 
 
 class PostView(DetailView):
